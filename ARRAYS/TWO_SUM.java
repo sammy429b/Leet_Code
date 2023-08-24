2adb57a2ebc96baaf5c1b047e331232754bb2a9c
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-08-24 08:19:03 +0530

    Two sum

---
+++ ARRAYS/TWO_SUM.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        int size = nums.length;
+        for(int i = 0; i < size-1; i++){
+            for(int j = i+1; j < size; j++){
+                if(nums[i] + nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
