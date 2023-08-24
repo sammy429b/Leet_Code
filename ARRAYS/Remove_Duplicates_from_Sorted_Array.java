71d268d5b36e312957de1f7c56fa024a09d58788
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-08-24 08:37:19 +0530

    Remove_Duplicates_from_Sorted_Array

---
+++ ARRAYS/Remove_Duplicates_from_Sorted_Array.java
@@ -0,0 +1,14 @@
+public class Remove_Duplicates_from_Sorted_Array {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int count = 1;
+            for(int i = 1; i < nums.length; i++){
+                if(nums[i]!=nums[count-1]){
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
