4117f99570607a4cbc12e68329c2cef07b34a478
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-10-06 19:33:59 +0530

    Remove Element

--- ARRAYS/TWO_SUM.java
+++ ARRAYS/TWO_SUM.java
@@ -1,17 +1,19 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int size = nums.length;
-        for(int i = 0; i < size-1; i++){
-            for(int j = i+1; j < size; j++){
-                if(nums[i] + nums[j] == target){
-                    result[0] = i;
-                    result[1] = j;
-                    break;
+public class TWO_SUM {
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int[] result = new int[2];
+            int size = nums.length;
+            for (int i = 0; i < size - 1; i++) {
+                for (int j = i + 1; j < size; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                        break;
+                    }
+                    ;
                 }
-                ;
             }
+            return result;
         }
-        return result;
     }
 }
