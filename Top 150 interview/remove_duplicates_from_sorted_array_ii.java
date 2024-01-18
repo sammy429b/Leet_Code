0e580a0450ebed6b04b747020dc1acdd2a923be2
sammy429b <samarthnbahirgonde49@gmail.com>
2024-01-18 14:29:21 +0530

    remove-duplicates-from-sorted-array-ii

---
+++ Top 150 interview/remove_duplicates_from_sorted_array_ii.java
@@ -0,0 +1,19 @@
+public class remove_duplicates_from_sorted_array_ii {
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int size = nums.length;
+            int count = 1;
+            int j = 1;
+            for (int i = 1; i < size; i++) {
+                if (nums[i] == nums[i - 1]) {
+                    count++;
+                } else
+                    count = 1;
+                if(count<=2){
+                    nums[j++] = nums[i];
+                }
+            }
+            return j;
+        }
+    }
+}
