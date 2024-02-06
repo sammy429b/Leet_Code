66b4f11fd7a25e3523a5028587ec146a8ff0caf6
sammy429b <samarthnbahirgonde49@gmail.com>
2024-02-06 14:21:15 +0530

    27.remove element

---
+++ Top 150 interview/Remove_element.java
@@ -0,0 +1,22 @@
+public class Remove_element {
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int k = 0;  // Pointer to the next available position to overwrite
+            
+            // Iterate through each element of the array
+            for (int i = 0; i < nums.length; i++) {
+                // If the current element is not equal to the specified value
+                if (nums[i] != val) {
+                    // Copy the current element to position k and increment k
+                    nums[k++] = nums[i];
+                }
+                // If the current element is equal to the specified value,
+                // do nothing as we're removing it from the array
+            }
+            
+            // Return the length of the modified array without the removed elements
+            return k;
+        }
+    }
+    
+}
